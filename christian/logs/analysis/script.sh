8caa2484d0fee240f0ff4b95abe89c6a0fb59142
Christian Sattler <sattler.christian@gmail.com>
2016-01-29 18:38:44 +0000

    Added tentative packet format description.

--- christian/logs/analysis/script.sh
+++ christian/logs/analysis/script.sh
@@ -4,7 +4,7 @@ INPUT="$1"
 
 cd "analysis"
 mkdir "$INPUT"
-tshark -r "../$INPUT" -2R 'usb.data_len == 18 && usb.transfer_type == 0x01 && usb.endpoint_number == 0x81' -T fields -e usb.capdata >"$INPUT/cap-data"
+tshark -r "../$INPUT" -2R 'usb.data_len == 18 && usb.transfer_type == 0x01 && usb.endpoint_number == 0x81 && usb.capdata[0] == 0x02' -T fields -e usb.capdata >"$INPUT/cap-data"
 cd "$INPUT"
 cat cap-data | sed 's/^02://' | sed 's/:00:00:00:00:00:00:00:00:00:00:00//' >six-bytes
 cat six-bytes | sed '$!N; /^\(.*\)\n\1$/!P; D' >six-bytes-unique
